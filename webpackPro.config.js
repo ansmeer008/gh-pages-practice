f54fca5ac405b2ef15a309f2dab795d271fc32e9
ansmeer008 <ansmeer008@gamil.com>
2022-11-24 15:03:27 +0900

    리액트 번들링 연습

---
+++ webpackPro.config.js
@@ -0,0 +1,44 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "[name].js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /.s?css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+    runtimeChunk: "single", //optimization 자체에 적용가능한 옵션인 것 같은데 기본적으로 multiple이고 single로 설정하면
+    //The value 'single' instead creates a runtime file to be shared for all generated chunks.
+    //https://webpack.js.org/configuration/optimization/#optimizationruntimechunk
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  devServer: {
+    static: "./dist",
+  },
+};
